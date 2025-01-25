1d6e2d137e0a6bec7fcd23fcbe6657322114be80
Felipe <pennafelipe00@gmail.com>
2025-01-25 13:50:36 -0300

    primeiro commit

---
+++ calculadora.py
@@ -0,0 +1,16 @@
+def adicao (a, b):
+    return a + b
+
+def subtracao (a, b):
+    return a - b;
+
+def multiplicacao (a, b):
+    return a * b
+
+def verDivisao (a, b):
+    if b == 0:
+        raise ValueError ("NAO PODE DIVIDIR POR ZERO")
+
+def divisao (a, b):
+    verDivisao(a, b)
+    return a / b
